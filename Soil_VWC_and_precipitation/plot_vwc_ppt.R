a82705db9ac0904db6f84262bbc34454715f51a9
Ava Hoffman <avamariehoffman@gmail.com>
2020-03-17 11:41:53 -0400

    Annotate vwc_ppt plotting function

--- Soil_VWC_and_precipitation/plot_vwc_ppt.R
+++ Soil_VWC_and_precipitation/plot_vwc_ppt.R
@@ -20,18 +20,24 @@ theme_sigmaplot <-
     # This function adds Sigma-plot like theme elements to a ggplot object.
     # Use as an additional arg, eg:
     # ggplot() + theme_sigmaplot()
-    # 
+    #
     # ticklen: how long ticks (interior) will be
     
     sigmaplot <-
       theme(
-        panel.background = element_blank(), # Clear background
-        panel.border = element_rect(size = 1, fill = NA), # Full border around plot
-        legend.key = element_rect(fill = NA), # Each legend item (e.g., lines) doesn't 
+        panel.background = element_blank(),
+        # Clear background
+        panel.border = element_rect(size = 1, fill = NA),
+        # Full border around plot
+        legend.key = element_rect(fill = NA),
+        # Each legend item (e.g., lines) doesn't
         # have a background
-        axis.ticks.length.y = unit(ticklen, "cm"), # Tick size
-        axis.ticks.length.y.right = unit(ticklen, "cm"), # Tick size
-        axis.ticks.length.x = unit(ticklen, "cm"), # Tick size
+        axis.ticks.length.y = unit(ticklen, "cm"),
+        # Tick size
+        axis.ticks.length.y.right = unit(ticklen, "cm"),
+        # Tick size
+        axis.ticks.length.x = unit(ticklen, "cm"),
+        # Tick size
         # X tick labels are given some padding
         axis.text.x = element_text(
           color = "black",
@@ -100,7 +106,7 @@ scale_y_custom_ticks <-
     # This function adds a custom y scale element to a ggplot object.
     # Use as an additional arg, eg:
     # ggplot() + scale_y_custom_ticks()
-    # 
+    #
     # scale_factor: Factor by which second y axis will differ (eg., different scales)
     # HYS: boolean, whether the site is HYS (HYS will have larger axis limits and more ticks)
     
@@ -108,60 +114,78 @@ scale_y_custom_ticks <-
       # If site is CHY
       obj <-
         scale_y_continuous(
-          name = "VWC %", # Title of left axis
-          limits = c(0, 0.3), # Limits of both axes (based off left side)
+          name = "VWC %",
+          # Title of left axis
+          limits = c(0, 0.3),
+          # Limits of both axes (based off left side)
           breaks = c(0,
                      0.1,
                      0.2,
-                     0.3), # Left breaks
-          expand = c(0, 0), # Removes padding around axes
+                     0.3),
+          # Left breaks
+          expand = c(0, 0),
+          # Removes padding around axes
           labels = c(0,
                      0.1,
                      0.2,
-                     0.3), # Left break labels
+                     0.3),
+          # Left break labels
           sec.axis = sec_axis(
-            ~ . * scale_factor, # Apply scaling factor to second axis
-            name = "Ambient precipitation (mm)", # Title of right axis
+            ~ . * scale_factor,
+            # Apply scaling factor to second axis
+            name = "Ambient precipitation (mm)",
+            # Title of right axis
             breaks = c(0,
                        30,
                        60,
-                       90), # Right breaks
+                       90),
+            # Right breaks
             labels = c(0,
                        30,
                        60,
-                       90), # Right break labels
+                       90),
+            # Right break labels
           )
         )
     } else {
       # If site is HYS
       obj <-
         scale_y_continuous(
-          name = "VWC %", # Title of left axis
-          limits = c(0, 0.45), # Limits of both axes (based off left side)
+          name = "VWC %",
+          # Title of left axis
+          limits = c(0, 0.45),
+          # Limits of both axes (based off left side)
           breaks = c(0,
                      0.1,
                      0.2,
                      0.3,
-                     0.4), # Left breaks
-          expand = c(0, 0), # Removes padding around axes
+                     0.4),
+          # Left breaks
+          expand = c(0, 0),
+          # Removes padding around axes
           labels = c(0,
                      0.1,
                      0.2,
                      0.3,
-                     0.4), # Left break labels
+                     0.4),
+          # Left break labels
           sec.axis = sec_axis(
-            ~ . * scale_factor, # Apply scaling factor to second axis
-            name = "Ambient precipitation (mm)", # Title of right axis
+            ~ . * scale_factor,
+            # Apply scaling factor to second axis
+            name = "Ambient precipitation (mm)",
+            # Title of right axis
             breaks = c(0,
                        30,
                        60,
                        90,
-                       120), # Right breaks
+                       120),
+            # Right breaks
             labels = c(0,
                        30,
                        60,
                        90,
-                       120), # Right break labels
+                       120),
+            # Right break labels
           )
         )
     }
@@ -175,13 +199,17 @@ scale_x_custom_ticks <-
     # This function adds a custom x scale element to a ggplot object.
     # Use as an additional arg, eg:
     # ggplot() + scale_x_custom_ticks()
-
+    
     obj <-
       scale_x_date(
-        limits = as.Date(c('2017-04-01', '2017-09-15')), # Axis limits as dates
-        expand = c(0, 0), # Remove padding around axes
-        date_breaks = "month", # Label each month
-        date_labels = "%b", # Use the Julian month
+        limits = as.Date(c('2017-04-01', '2017-09-15')),
+        # Axis limits as dates
+        expand = c(0, 0),
+        # Remove padding around axes
+        date_breaks = "month",
+        # Label each month
+        date_labels = "%b",
+        # Use the Julian month
         name = "" # No title needed on the x axis
       )
     
@@ -197,7 +225,8 @@ config <-
     
     return(
       list(
-        year_to_plot = 2017, ## 2014 - 2017, eg
+        year_to_plot = 2017,
+        ## 2014 - 2017, eg
         bar_color = "#162947",
         drt_color = "#de9f40",
         con_color = "#3e946c",
@@ -215,7 +244,7 @@ prep_data <-
            vwc45) {
     # This function cleans the soil VWC and precipitation event data and prepares
     # it for plotting
-    # 
+    #
     # HYS: boolean, T = site is HYS (HYS will have larger axis limits and more ticks)
     # chr_only: boolean, T = only take the chronic treatment. Otherwise averages
     # chronic and intense treatments as "drought"
@@ -299,6 +328,16 @@ make_vwc_ppt_plot <- function(# Data items
   legend_title,
   leg_position = c(0.75, 0.8),
   save_file = save_file) {
+  # This function plots the data
+  #
+  # HYS: boolean, T = site is HYS (HYS will have larger axis limits and more ticks)
+  # chr_only: boolean, T = only take the chronic treatment. Otherwise averages
+  # chronic and intense treatments as "drought"
+  # vwc45: boolean, T = takes the field "vwc45". Otherwise takes "vwc90"
+  # legend_title: String for name of the legend or the plot overall
+  # leg_position: Coordinates to indicate position of the legend inside the plot
+  # save_file: String of the path and file name where the file is to be saved
+  
   # Establish some constants, per the config function
   year_to_plot <- config()[[1]]
   bar_color <- config()[[2]]
@@ -306,65 +345,71 @@ make_vwc_ppt_plot <- function(# Data items
   con_color <- config()[[4]]
   scale_factor <- config()[[5]]
   
+  # Gather data for plotting
   tmp <-
     prep_data(HYS,
               chr_only,
               vwc45)
-  
   vwc_plot <- tmp[[1]]
   ppt <- tmp[[2]]
   
+  # Make plot object
   gg <-
     ggplot() +
+    # Precipitation
     geom_col(
       data = ppt,
       aes(x = date, y = `ambient precip (mm)` / scale_factor),
-      fill = bar_color
+      # Make sure data is scaled accordingly
+      fill = bar_color # Color the bars accordingly
     ) +
+    # Soil VWC%
     geom_line(data = vwc_plot,
               aes(
                 x = TIMESTAMP,
                 y = VWC,
                 group = Treatment,
-                color = Treatment
+                # Different colors per treatment
+                color = Treatment # Different colors per treatment
               )) +
     scale_y_custom_ticks(scale_factor,
-                         HYS = HYS) +
-    scale_x_custom_ticks() +
+                         HYS = HYS) + # Add custom ticks for y axes depending on site
+    scale_x_custom_ticks() + # Customize x ticks
     scale_color_manual(
       values = c(drt_color, con_color),
+      # Line colors
       labels = c("Drought", "Control"),
-      name = legend_title
+      # Line labels for treatments
+      name = legend_title # Legend title
     ) +
-    theme_sigmaplot() +
+    theme_sigmaplot() + # General themeing / padding
     theme(
+      # Additional themeing for formatting legend
       legend.position = leg_position,
       legend.title.align = 0.5,
-      legend.box.background = element_rect(size = 1, fill = NA)
+      # Center justify text of the legend title
+      legend.box.background = element_rect(size = 1, fill = NA) # Box around the legend
     )
   
+  # Color the ticks specifically so that the top and bottom are not overlapping with the
+  # box surrounding the overall plot (looks weird and sloppy if this happens)
   if (!(HYS)) {
     gg <-
       gg +
       theme(axis.ticks.y = element_line(color = c(
         "transparent",
-        "black",
-        "black",
+        rep("black", 2),
         "transparent"
       )))
   } else {
     gg <-
       gg +
-      theme(axis.ticks.y = element_line(color = c(
-        "transparent",
-        "black",
-        "black",
-        "black",
-        "black"
-      )))
+      theme(axis.ticks.y = element_line(color = c("transparent",
+                                                  rep("black", 4))))
   }
   
   gg
+  # Save plot
   ggsave(file = save_file,
          width = 6,
          height = 4)
